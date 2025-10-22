f286f27755b170d30efff27863861a796a9af071
Rierini <narutoissatoshi@gmail.com>
2025-10-22 20:26:55 +0200

    continue from where i left AOV-analys

--- src/io_utils.py
+++ src/io_utils.py
@@ -1,10 +1,1 @@
 import pandas as pd
-
-    # Laddar e-handelsdata från CSV-fil
-    # Argument:
-    #     filvag: Sökväg till CSV-filen
-    # Returnerar:
-    #     DataFrame med sales data
-
-def ladda_data(filvag):
-    return pd.read_csv(filvag)

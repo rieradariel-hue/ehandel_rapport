7488aedaee034676508af5ba4e955a9be3b856f6
Henri1979 <58734078+Henri1979@users.noreply.github.com>
2025-10-23 10:55:10 +0200

    added total rev and units (#25)

---
+++ src/ecommerce.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import pandas as pd
+
+base_dir = Path(__file__).parent      # src/
+csv_path = base_dir.parent / "data" / "ecommerce_sales.csv"
+
+df = pd.read_csv(csv_path)
+
+total_revenue = df["revenue"].sum()
+total_units = df["units"].sum()
+
+# print(int(round(total_revenue)))
